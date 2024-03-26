b19e64f84533bf1cab6161af3884b422dd04cebe
Chocobotar <penguinprinny@yahoo.com>
2024-03-26 10:04:18 +0900

    Log language

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         </h1>
       </center>
       <StartBlock />
+      <div>{userLanguage}</div>
       {userLanguage === "ru" ? (
         <> {/* Russian */}
           <div>
