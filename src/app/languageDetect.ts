3575813b9fbff4a5cf3685a8c5f76b9d54ff6599
Chocobotar <penguinprinny@yahoo.com>
2024-03-26 10:15:09 +0900

    Log language continue 2

--- src/app/languageDetect.ts
+++ src/app/languageDetect.ts
@@ -1,7 +1,6 @@
 export function getUserLanguage(): string {
-    if (typeof navigator !== 'undefined') {
+
         const userLang = navigator.language;
         return userLang.split('-')[0]; // Extract the language code (e.g., 'en' from 'en-US')
-    }
-    return 'default'; // Fallback to a default language
+
 }
