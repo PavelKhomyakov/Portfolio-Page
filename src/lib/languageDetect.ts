95dfc2fbc3e5541aa269e3b9215cd7a078ee1577
Chocobotar <penguinprinny@yahoo.com>
2024-03-27 11:47:16 +0900

    Console log added

--- src/lib/languageDetect.ts
+++ src/lib/languageDetect.ts
@@ -1,12 +1,10 @@
-// utils/language.js
-
 export function getUserLanguage() {
     // ユーザーの言語を取得
     const userLang = process.env.LANG || process.env.LANGUAGE || process.env.LC_ALL || process.env.LC_MESSAGES;
   
     // 言語コードの形式を確認（例: 'en-US'）
-    const langCode = userLang ? userLang.split('_')[0] : 'default'; // デフォルトは英語
-  
+    const langCode = userLang ? userLang.split('_')[0] : 'ass'; // デフォルトは英語
+    console.log('Language:', userLang);
     return langCode;
   }
   
