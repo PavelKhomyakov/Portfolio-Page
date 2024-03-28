b9433464aa7b14915ec46d1263eb0a8bce142c01
Chocobotar <penguinprinny@yahoo.com>
2024-03-28 15:57:58 +0900

    Language experiment 8

--- src/lib/languageDetect.ts
+++ src/lib/languageDetect.ts
@@ -1,14 +1,9 @@
 export function getUserLanguage() {
-  // ユーザーの言語を取得
+
   if (typeof window !== 'undefined') {
     const userLang = window.navigator.language;
+    const langCode = userLang ? userLang.split('_')[0] : 'en';
 
-    // 言語コードの形式を確認（例: 'en-US'）
-    const langCode = userLang ? userLang.split('_')[0] : 'ass'; // デフォルトは英語
-    console.log(process.env.LANG);
-    console.log('Language:', userLang);
     return langCode;
-  } else {
-    return "ass"
   }
 }
